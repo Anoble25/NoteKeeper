62c8a9a3108632353962bd13d1adb4c087583770
ANoble25 <AWGR-Alex@comcast.net>
2018-06-26 09:05:13 -0700

    update recycler adapter

--- app/src/main/java/com/example/alex/notekeeper/NoteRecyclerAdapter.java
+++ app/src/main/java/com/example/alex/notekeeper/NoteRecyclerAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
 
@@ -21,7 +22,8 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -36,8 +38,13 @@ public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
         }
     }
 }
